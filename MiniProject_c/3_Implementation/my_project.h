052e230364592e11117124f6cb1049c3dcf8f312
ajvk7755 <69146897+ajvk7755@users.noreply.github.com>
2020-11-14 11:37:31 +0530

    Add files via upload

---
+++ MiniProject_c/3_Implementation/my_project.h
@@ -0,0 +1,17 @@
+/**
+* @file my_project.h
+*
+*/
+#ifndef __MY_PPROJECT_H__
+#define __MY_PPROJECT_H__
+
+/**
+* Finds if a mobile has the processor mentioned
+* Finds the if the devices list is found
+* @note Returns 0 if found
+*/
+
+int list_of_donor();
+int search_donor(char *bgrp);
+
+#endif /* #ifndef __MY_PPROJECT_H__ */
